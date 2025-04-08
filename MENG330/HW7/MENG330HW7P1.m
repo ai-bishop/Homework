3b796b79cfb51120fe5f75d9320c44cca32e6fbe
ai-bishop <artemisbishop1@gmail.com>
2025-04-08 10:29:13 -0700

    P1 complete
    completed problem one. solutions match with peers.

--- MENG330/HW7/MENG330HW7P1.m
+++ MENG330/HW7/MENG330HW7P1.m
@@ -1,6 +1,6 @@
 %% Initialize problem
 % create variables
-Sy = 420; % N/mm^2
+Sy = 420; % N/mm^2 aka MPA
 Sut = 560; % N/mm^2
 SafetyFactor = 2.5; % safety factor
 F_angle = 20; % degrees
@@ -68,9 +68,9 @@ tau_tot = ttorsion + tshear;
 vm_shear = sqrt(3 * tau_tot ^ 2);
 
 % Max moment
-sigma_de = M_max * (diam / 2) / inertia;
+sigma_mom = M_max * (diam / 2) / inertia;
 
-vm_moment = sqrt(sigma_de^2 + 3 * tau_tot^2);
+vm_moment = sqrt(sigma_mom^2 + 3 * tau_tot^2);
 
 % solve
 diam_s = double(vpasolve(Sy / vm_shear == SafetyFactor, diam, [0 inf]));
@@ -79,12 +79,92 @@ diam_m = double(vpasolve(Sy / vm_moment == SafetyFactor, diam, [0 inf]));
 diam_static = max(diam_s, diam_m)
 
 
-
-
-
-
-
-
-
 %% Part B: Mod-Goodman
+
+syms diam % everything is a function of diameter here
+
+% assumption: infinitie life desired
+% moment gives a larger force than shear as per Part A
+
+% calculate without stress concs
+sigma_m = sqrt(3*ttorsion^2);
+sigma_a_0 = sqrt(sigma_mom^2);
+% factor in stress concs later once have
+
+% stress conc @ shoulders + gear, T7-1
+K_t = 2.7;
+
+% conversion from metric to english as the q equations are in english
+mm_in = 1/25.4; % mm to inches conversion factor
+mpa_ksi = 0.145038; % ksi per mpa
+
+% assume r/d = 0.02 - justifiable as sharp radii value
+% find radius in inches
+rad_in = diam * 0.02 / mm_in; % in
+
+root_a = 0.246 - 3.08e-3 * (Sut * mpa_ksi) + 1.51e-5 * (Sut * mpa_ksi)^2 - 2.67e-8 * (Sut * mpa_ksi)^3; % in^0.5
+
+% calculate q, given equation
+q = 1 / (1 + root_a / sqrt(rad_in));
+
+% find K_f
+K_f = 1 + q * (K_t - 1);
+
+% find alternating stress at concentration
+sigma_a = K_f * sigma_a_0;
+
+
+% shaft is steel, so Se' = Sut / 2 if Sut < 1400 mpa
+Se_0 = Sut / 2;
+
+% find k factors
+% k factors are taken from textbook tables, also on slides
+% assume shaft is machined as not specified in problem
+k_a = 3.04 * Sut ^ (-0.217);
+
+% diameter ~ 32mm so falls in first range for k_b
+k_b = 1.51 * diam^-0.157;
+% k_b = (diam/7.62) ^ -0.107;
+
+% using von mises stress so k_c = 1
+k_c = 1;
+
+% no temperature stated - assume operating at room temperature
+k_d = 1;
+
+% no reliability mentioned - assume 50%
+k_e = 1;
+
+% no misc vals
+k_f = 1;
+
+k_tot = k_a * k_b * k_c * k_d * k_e * k_f;
+Se = k_tot * Se_0;
+
+diam_fatigue = vpasolve(sigma_a / Se + sigma_m / Sut == 1 / SafetyFactor, diam, [0 inf])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
