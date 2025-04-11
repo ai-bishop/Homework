b0fbdb0249523cc14ab7ba73318b9e599b56c24d
ai-bishop <artemisbishop1@gmail.com>
2025-04-11 13:13:32 -0700

    initialize P1,2
    Initialized values for problems 1 and 2 on hw8

---
+++ MENG330/HW8/MENG330HW8P1.m
@@ -0,0 +1,42 @@
+%% Shigley's P8.14
+
+%% initialize values
+
+
+E_steel = 300; % M psi
+h_steel = 2; % in
+E_ci = 14.5; % M psi
+h_ci = 1; % in
+
+% one bolt
+% one nut
+
+% bolt is steel 1/2 13 UNC
+
+
+%% Part 1
+% determine suitable length for the bolt, rounded up to the nearest quarter inch
+
+
+
+
+
+%% Part 2:
+% determine bolt stiffness
+% spring equivalent thingy
+
+
+
+
+
+
+
+
+
+%% Part 3
+% determine stiffness of the members
+% cumulative equivalent
+
+
+
+
---
+++ MENG330/HW8/MENG330HW8P2.m
@@ -0,0 +1,42 @@
+%% Shigley's 8.33
+
+
+%% Initialize values
+% non permanent bolt
+E_steel = 207; % GPA
+E_ci = 100; % GPA
+% in mm:
+A = 20;
+B = 20;
+C = 100;
+D = 250;
+E = 200;
+F = 300;
+
+N = 10; % N
+
+p_g = 6; % MPA
+
+% Bolt grade: ISO 9.8
+% Bolt Spec: M12 x 1.75
+
+
+
+
+
+% Find:
+L
+n_p
+n_l
+n_0
+
+
+
+
+
+
+
+
+
+
+
