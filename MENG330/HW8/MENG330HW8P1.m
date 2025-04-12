78587eafe85ea5f1a6ee49259cd33e07e48ca847
ai-bishop <artemisbishop1@gmail.com>
2025-04-12 13:32:54 -0700

    p1 complete
    problem one completed, problem 3 initialized

--- MENG330/HW8/MENG330HW8P1.m
+++ MENG330/HW8/MENG330HW8P1.m
@@ -3,10 +3,13 @@
 %% initialize values
 
 
-E_steel = 300; % M psi
+E_steel = 30; % M psi
 h_steel = 2; % in
 E_ci = 14.5; % M psi
 h_ci = 1; % in
+d = 0.5; % in
+l = 3; % in
+H = 7/16;
 
 % one bolt
 % one nut
@@ -17,26 +20,41 @@ h_ci = 1; % in
 %% Part 1
 % determine suitable length for the bolt, rounded up to the nearest quarter inch
 
-
-
+% reference Table A-31
+% nut height = 7/16in
+% L >= l + H = 3 7/16 in, need to round up
+L = 3.5; % in
 
 
 %% Part 2:
 % determine bolt stiffness
 % spring equivalent thingy
 
-
-
-
-
-
-
+L_t = 2 * d + 1/4;
+l_d = L - L_t;
+l_t = l - l_d;
+A_d = pi * d^2 / 4;
+A_t = 0.1419; % Table 8-2
+k_b = A_d * A_t * E_steel / (A_d * l_t + A_t * l_d) % Mlbf / in
 
 
 %% Part 3
 % determine stiffness of the members
 % cumulative equivalent
 
+% Top Steel Frustum:
+t = 1.5;
+D = 0.75;
+k_1 = 0.5774 * pi * E_steel * d / log((1.155 * t + D - d)*(D + d) / ((1.155 * t + D + d) * (D - d)));
 
+% Bottom Steel Frustum
+t = 0.5;
+D = 0.75 + 2 * tand(30);
+k_2 = 0.5774 * pi * E_steel * d / log((1.155 * t + D - d)*(D + d) / ((1.155 * t + D + d) * (D - d)));
 
+% Cast Iron Frustum
+t = 1;
+D = 0.75;
+k_3 = 0.5774 * pi * E_ci * d / log((1.155 * t + D - d)*(D + d) / ((1.155 * t + D + d) * (D - d)));
 
+k_m = ( 1 / k_1 + 1 / k_2 + 1 / k_3)^(-1)
---
+++ MENG330/HW8/MENG330HW8P3.m
@@ -0,0 +1,67 @@
+%% Shigley's 8.76
+
+
+
+% vertical channel 152x76
+% cantilever beam
+% channel: AISI 1006 HR
+% bar: AISI 1015 HR
+% shoulder bolts: M10x1.5 ISO 5.8
+% assume bolt threads dont extend into the joint
+% n = 2
+% find F_min, lowest safe force that can be applied to the cantilever
+
+% distances, mm
+AO = 50;
+OB = 50;
+BC = 26;
+CF = 125;
+width = 50;
+thickness = 12;
+
+
+
+
+
+
+
+
+%% Find:
+% F_min
+% shear on bolt
+% bearing on bolt
+% bearing on channel
+% bearing cantilver
+% bending of cantilever
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
