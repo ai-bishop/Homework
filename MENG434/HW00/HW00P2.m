b7cb1d71940b0bbc858f2993dd13e9e0c22374ee
ai-bishop <artemisbishop1@gmail.com>
2025-09-03 11:14:01 -0700

    Create HW00P2.m

---
+++ MENG434/HW00/HW00P2.m
@@ -0,0 +1,14 @@
+% This file is used for the graphing of HW00P2
+
+y = @(t) (2 / 401) * (0.5 * cos(10 * t) + 10 * sin(10 * t)) + (400/401) * exp(-t/2);
+
+
+% plotting
+figure 
+hold on
+grid on
+
+% plot function
+t = 0:0.1:10;
+plot(y(t))
+xlim([0,10])
